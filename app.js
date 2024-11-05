3ee7f850eedd9a283a2f42fd8a247e4b3f953634
Chelsea <cberrios5114@gmail.com>
2024-11-05 09:41:36 -0500

    message

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const firebase = require("firebase/app");
+const app = express();
+const port = 3000;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7eawe4eiO0l57te5xqy7obF2BLfgLdRQ",
+  authDomain: "exercise-five-733a7.firebaseapp.com",
+  projectId: "exercise-five-733a7",
+  storageBucket: "exercise-five-733a7.firebasestorage.app",
+  messagingSenderId: "937743119909",
+  appId: "1:937743119909:web:efa232ff4ee8c4de55f1cc",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const indexRoute = require("./routes/index.js");
+const createPostRoute = require("./routes/createPost.js");
+const singlePostRoute = require("./routes/singlePost.js");
+
+app.use("/", indexRoute);
+app.use("/create", createPostRoute);
+app.use("/post", singlePostRoute);
+
+app.listen(port, () => {
+  console.log(`Exercise Five app listening on port ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
+
+router.get("/", (request, response) => {
+  const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+  const postsArray = [];
+
+  postsQuery
+    .then((res) => {
+      res.forEach((post) => {
+        if (!post) response.send("Data does not exist");
+        postsArray.push({ id: post.id, ...post.data() });
+      });
+      response.send(postsArray);
+    })
+    .catch((error) => {
+      console.log(error);
+      return response.send(error);
+    });
+});
+
+module.exports = router;
