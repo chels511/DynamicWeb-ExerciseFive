468cb454db3be96d6558b75144539c179eb6d2b4
Chelsea <cberrios5114@gmail.com>
2024-11-09 21:38:55 -0500

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7eawe4eiO0l57te5xqy7obF2BLfgLdRQ",
+  apiKey: process.env.FIREBASE_KEY,
   authDomain: "exercise-five-733a7.firebaseapp.com",
   projectId: "exercise-five-733a7",
   storageBucket: "exercise-five-733a7.firebasestorage.app",
@@ -22,7 +22,10 @@ const singlePostRoute = require("./routes/singlePost.js");
 app.use("/", indexRoute);
 app.use("/create", createPostRoute);
 app.use("/post", singlePostRoute);
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Exercise Five app listening on port ${port}`);
 });
+
+module.exports = app;
--- routes/createPost.js
+++ routes/createPost.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
-router.get("/", (request, response) => {
-  response.send("Create Post Route");
-});
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
 
+router.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "../public", "form.html"));
+});
+router.get("/submit", (request, response) => {
+  const { title, postText, author } = request.query;
+  if (!title || !postText || !author) {
+    response.send({ error: "Invalid Form Submission" });
+  }
+  const idFromTitle = title.replace(/\s+/g, "-").toLowerCase();
+  const setBlogPost = firestore.setDoc(
+    firestore.doc(db, "posts", idFromTitle),
+    {
+      title: title,
+      text: postText,
+      author: author,
+    }
+  );
+  setBlogPost
+    .then((res) => {
+      response.sendFile(path.join(__dirname, "../public", "success.html"));
+    })
+    .catch((error) => {
+      response.send(`Error Submitting: ${error.toString()}`);
+    });
+
+  //response.send(idFromTitle);
+});
 module.exports = router;
